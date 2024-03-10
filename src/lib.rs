327ce8f0081b509cdc5c667ce0bf5400a868fa0b
Jaquior-qiao <Jaquior-qiao@github.com>
2024-03-10 13:23:09 +0800

    final test ,endpoint,and some chages

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::result::Result;
+use std::{net::TcpListener, result::Result};
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 
@@ -6,12 +6,12 @@ async fn health_check(_req: HttpRequest)-> HttpResponse{
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error>{
+pub fn run(listener:TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(||{
         App::new()
             .route("/health_check", web::get().to(health_check))
         })
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 //!main.rs
+use std::net::TcpListener;
+
 use Zero2pro::run;
 
 #[tokio::main]
 async fn main() ->std::io::Result<()>{
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Fail to build a listener");
+    run(listener)?.await
 }
